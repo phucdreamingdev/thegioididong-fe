353af00c831b599eef400c37ed597a771ec88e0e
= <phuc.dreamingdev@gmail.com>
2022-06-21 11:21:45 +0700

    add structure of project

---
+++ src/main/java/vn/com/groupfive/tgdd/GroupfiveTgddFeApplication.java
@@ -0,0 +1,13 @@
+package vn.com.groupfive.tgdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupfiveTgddFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupfiveTgddFeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/com/groupfive/tgdd/GroupfiveTgddFeApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.groupfive.tgdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupfiveTgddFeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
