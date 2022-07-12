d8da86be0126a64f260430c2420d8a8f813be1d7
giaminh88 <minh.ldg88@gmail.com>
2022-07-12 18:03:55 +0700

    Render Category API

--- src/main/java/vn/com/groupfive/tgdd/GroupfiveTgddFeApplication.java
+++ src/main/java/vn/com/groupfive/tgdd/GroupfiveTgddFeApplication.java
@@ -2,12 +2,19 @@ package vn.com.groupfive.tgdd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class GroupfiveTgddFeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GroupfiveTgddFeApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
--- src/main/java/vn/com/groupfive/tgdd/controller/HomeController.java
+++ src/main/java/vn/com/groupfive/tgdd/controller/HomeController.java
@@ -1,53 +1,30 @@
 package vn.com.groupfive.tgdd.controller;
 
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+
 
 @Controller
 public class HomeController {
 
-	@GetMapping({ "/", "/home", "/index" })
-	public String home() {
-		return "index";
-	}
+  @Autowired
+  RestTemplate restTemplate;
 
-	@GetMapping("/dtdd")
-	public String phonePage() {
-		return "pages/category/dtdd";
-	}
+  @RequestMapping(value = { "/", "/home", "/index" })
+  public String index(Model model) {
+    RestTemplate restTemplate = new RestTemplate();
+    String fooResourceUrl = "http://localhost:8001/customer/get-all-category";
+    ResponseEntity<Object> response = restTemplate.getForEntity(fooResourceUrl, Object.class);
+    model.addAttribute("categories", response.getBody());
+    return "index";
+  }
 
-	@GetMapping("/laptop-ldp")
-	public String laptopPage() {
-		return "pages/category/laptop-ldp";
-	}
-
-	@GetMapping("/may-tinh-bang")
-	public String tabletPage() {
-		return "pages/category/tablet";
-	}
-
-	@GetMapping("/phu-kien")
-	public String phukienPage() {
-		return "pages/category/phu-kien";
-	}
-
-	@GetMapping("/dong-ho-thong-minh-ldp")
-	public String smartWatchPage() {
-		return "pages/category/dong-ho-thong-minh";
-	}
-
-	@GetMapping("/dong-ho")
-	public String watchPage() {
-		return "pages/category/dong-ho";
-	}
-
-	@GetMapping("/pc-may-in")
-	public String pcmayinPage() {
-		return "pages/category/pc-may-in";
-	}
-
-	@GetMapping("/may-doi-tra")
-	public String maydoitraPage() {
-		return "pages/may-doi-tra";
-	}
 }
