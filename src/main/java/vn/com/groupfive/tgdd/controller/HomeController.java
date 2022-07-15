475c9d7f3701674b5b615038695155b8648aa1ca
giaminh88 <minh.ldg88@gmail.com>
2022-07-15 09:04:45 +0700

    Update header UI

--- src/main/java/vn/com/groupfive/tgdd/controller/HomeController.java
+++ src/main/java/vn/com/groupfive/tgdd/controller/HomeController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
@@ -36,13 +35,22 @@ public class HomeController {
     return "index";
   }
 
-  @GetMapping("/category/{id}")
+  @RequestMapping("/category/{id}")
   public String productList(Model model) {
     RestTemplate restTemplate = new RestTemplate();
+    String resourceUrl0 = "http://localhost:8001/customer/get-all-category-by-level/0";
+    String resourceUrl1 = "http://localhost:8001/customer/get-all-category-by-level/1";
+    String resourceUrl2 = "http://localhost:8001/customer/get-all-category-by-level/2";
     String resourceProductUrl = "http://localhost:8001/customer/get-all-products";
 
+    ResponseEntity<Object> response0 = restTemplate.getForEntity(resourceUrl0, Object.class);
+    ResponseEntity<Object> response1 = restTemplate.getForEntity(resourceUrl1, Object.class);
+    ResponseEntity<Object> response2 = restTemplate.getForEntity(resourceUrl2, Object.class);
     ResponseEntity<Object> productsResponse = restTemplate.getForEntity(resourceProductUrl, Object.class);
 
+    model.addAttribute("categoriesLevel0", response0.getBody());
+    model.addAttribute("categoriesLevel1", response1.getBody());
+    model.addAttribute("categoriesLevel2", response2.getBody());
     model.addAttribute("products", productsResponse.getBody());
 
     return "/pages/layout-products";
