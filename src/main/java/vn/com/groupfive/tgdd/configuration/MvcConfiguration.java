edaba27b7c50688d26ddc8f3bdc7b42267bd4b88
giaminh88 <minh.ldg88@gmail.com>
2022-07-23 15:59:49 +0700

    Update Admin UI

---
+++ src/main/java/vn/com/groupfive/tgdd/configuration/MvcConfiguration.java
@@ -0,0 +1,23 @@
+package vn.com.groupfive.tgdd.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry viewControllerRegistry) {
+        viewControllerRegistry.addViewController("/admin").setViewName("redirect:/admin/dashboard");
+    }
+
+}
--- src/main/java/vn/com/groupfive/tgdd/controller/AdminController.java
+++ src/main/java/vn/com/groupfive/tgdd/controller/AdminController.java
@@ -10,27 +10,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
-@RequestMapping("/admin")
+@RequestMapping(value = "admin")
 public class AdminController {
 
-	@Autowired
-	RestTemplate restTemplate;
-
-	@GetMapping({ "", "home" })
+	@GetMapping(value = "dashboard")
 	public String adminHome(Model model) {
-
-		model.addAttribute("admin", true);
 		return "admin/fragments/dashboard";
 	}
 
-	@GetMapping("categories-add")
+	@GetMapping(value = "categories/add")
 	public String categoryAdd(Model model) {
-		model.addAttribute("category", true);
 		return "admin/fragments/category/category-add";
 	}
 
-	@GetMapping("categories-list")
+	@GetMapping(value = "categories/list")
 	public String categoryList(Model model) {
+		RestTemplate restTemplate = new RestTemplate();
+
 		String resourceUrl0 = "http://localhost:8001/customer/get-all-category-by-level/0";
 		String resourceUrl1 = "http://localhost:8001/customer/get-all-category-by-level/1";
 		String resourceUrl2 = "http://localhost:8001/customer/get-all-category-by-level/2";
@@ -47,111 +43,76 @@ public class AdminController {
 		return "admin/fragments/category/category-list";
 	}
 
-	@GetMapping("product-add")
+	@GetMapping(value = "product/add")
 	public String addProduct(Model model) {
-		model.addAttribute("product", true);
+
 		return "admin/fragments/products/products-add";
 	}
 
-	@GetMapping("product-list")
+	@GetMapping(value = "product/list")
 	public String listProduct(Model model) {
-		model.addAttribute("product", true);
+
 		return "admin/fragments/products/products-list";
 	}
 
-	@GetMapping("branch-list")
+	@GetMapping(value = "branch/list")
 	public String listBranch(Model model) {
-		model.addAttribute("branch", true);
+
 		return "admin/fragments/branch/branch-list";
 	}
 
-	@GetMapping("branch-add")
+	@GetMapping(value = "branch/add")
 	public String addBranch(Model model) {
-		model.addAttribute("branch", true);
+
 		return "admin/fragments/branch/branch-add";
 	}
 
-	@GetMapping("order-list")
+	@GetMapping(value = "order/list")
 	public String listOrder(Model model) {
-		model.addAttribute("order", true);
+
 		return "admin/fragments/order/order-list";
 	}
 
-	@GetMapping("order-add")
+	@GetMapping(value = "order/add")
 	public String addOrder(Model model) {
-		model.addAttribute("order", true);
+
 		return "admin/fragments/order/order-add";
 	}
 
-	@GetMapping("promotion-list")
+	@GetMapping(value = "promotion/list")
 	public String listPromotion(Model model) {
-		model.addAttribute("promotion", true);
+
 		return "admin/fragments/promotion/promotion-list";
 	}
 
-	@GetMapping("promotion-add")
+	@GetMapping(value = "promotion/add")
 	public String addPromotion(Model model) {
-		model.addAttribute("promotion", true);
+
 		return "admin/fragments/promotion/promotion-add";
 	}
 
-	@GetMapping("transaction-list")
+	@GetMapping(value = "transaction/list")
 	public String listTransaction(Model model) {
-		model.addAttribute("transaction", true);
+
 		return "admin/fragments/transaction/transaction-list";
 	}
 
-	@GetMapping("transaction-add")
+	@GetMapping(value = "transaction/add")
 	public String addTransaction(Model model) {
-		model.addAttribute("transaction", true);
+
 		return "admin/fragments/transaction/transaction-add";
 	}
 
-	@GetMapping("member-list")
+	@GetMapping(value = "member/list")
 	public String listMember(Model model) {
-		model.addAttribute("member", true);
+
 		return "admin/fragments/member/member-list";
 	}
 
-	@GetMapping("member-add")
+	@GetMapping(value = "member/add")
 	public String addMember(Model model) {
-		model.addAttribute("member", true);
+
 		return "admin/fragments/member/member-add";
-	}
-
-	@ModelAttribute("dashboard")
-	public boolean dashboard() {
-		return false;
-	}
-
-	@ModelAttribute("product")
-	public boolean product() {
-		return false;
-	}
-
-	@ModelAttribute("branch")
-	public boolean branch() {
-		return false;
-	}
-
-	@ModelAttribute("transaction")
-	public boolean transaction() {
-		return false;
-	}
-
-	@ModelAttribute("promotion")
-	public boolean promotion() {
-		return false;
-	}
-
-	@ModelAttribute("order")
-	public boolean order() {
-		return false;
-	}
-
-	@ModelAttribute("member")
-	public boolean member() {
-		return false;
 	}
 
 }
