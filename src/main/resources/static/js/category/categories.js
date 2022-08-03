9195ae89f76cb47ac6344bd6a5922cfd4ea1979a
Phuc <phuc.dreamingdev@gmail.com>
2022-08-03 17:29:22 +0700

    Update Category Add

--- src/main/resources/static/js/category/categories.js
+++ src/main/resources/static/js/category/categories.js
@@ -1,32 +1,66 @@
+$(function () {
+  $("#level0").attr("disabled", "disabled");
+  $("#level1").attr("disabled", "disabled");
+  $("#level2").attr("disabled", "disabled");
+
+  $("#statusLv0T").attr("checked", "checked");
+  $("#statusLv1T").attr("checked", "checked");
+  $("#statusLv2T").attr("checked", "checked");
+
+  $("#isParent0T").attr("checked", "checked");
+  $("#isParent1T").attr("checked", "checked");
+});
 
 $(function () {
-  $("#level").change(function () {
-	var s = '<option value=' + -1 + '>SELECT</option>';
-	var s1 = '<option class="form-control" th:each="categories0 : ${categoriesLevel0}" th:value="${categories0.name}" th:text="${categories0.name}" id="parentNameLevel0"></option>';
-	var s2 = '<option class="form-control" th:each="categories1 : ${categoriesLevel1}" th:value="${categories1.name}" th:text="${categories1.name}" id="parentNameLevel1"></option>';
-    if ($(this).val() == 0) {
-      $("#parentNameDisplay").hide();
-      $("#idParentDisplay").show();
-      $("#childrenLevelDisplay").show();
-      $("#childrenLevel").removeAttr("disabled");
-      // $('input[type="radio"][name=isParent][value="no').prop("disabled", false);
-
-    } else if ($(this).val() == 1) {
-      $("#parentNameDisplay").show();
-      $("#idParentDisplay").show();
-      $("#childrenLevelDisplay").show();
-      $("#childrenLevel").attr("disabled", "disabled");
-      s += s1;
-	$('#parentName').html(s);
-
-    } else {
-      $("#idParentDisplay").hide();
-      $("#childrenLevelDisplay").hide();
-      s += s1;
-      $('#parentName').html(s);
+  $("#isParent0 input[type=radio]").change(function () {
+    if (this.value == 1) {
+      $("#childrenLevel0").attr("disabled", false);
+      $("#childrenLevel0Display").show();
+    } else if (this.value == 0) {
+      $("#childrenLevel0").attr("disabled", true);
+      $("#childrenLevel0Display").hide();
     }
-    $('#parentName').html(s);
   });
-  $("#status").attr('checked', 'checked');
-  $("#isParent").attr('checked', 'checked');
 });
+
+$(function () {
+  $("#isParent1 input[type=radio]").change(function () {
+    if (this.value == 1) {
+      $("#childrenLevel1Display").show();
+    } else if (this.value == 0) {
+      $("#childrenLevel1Display").hide();
+    }
+  });
+});
+formCategoryLv0
+
+$(function () {
+  $("#btnLevel0").click(function () {
+    $("#formCategoryLv0").show();
+    $("#formCategoryLv1").hide();
+    $("#formCategoryLv2").hide();
+  });
+  $("#btnLevel1").click(function () {
+    $("#formCategoryLv0").hide();
+    $("#formCategoryLv1").show();
+    $("#formCategoryLv2").hide();
+  });
+  $("#btnLevel2").click(function () {
+    $("#formCategoryLv0").hide();
+    $("#formCategoryLv1").hide();
+    $("#formCategoryLv2").show();
+  });
+
+  window.onload = function () {
+    $("#formCategoryLv0").hide();
+    $("#formCategoryLv1").hide();
+    $("#formCategoryLv2").hide();
+  }
+
+  $(document).ready(function () {
+    $("#formCategoryLv0").hide();
+    $("#formCategoryLv1").hide();
+    $("#formCategoryLv2").hide();
+  });
+});
+
