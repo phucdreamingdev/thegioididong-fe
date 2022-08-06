6826345ed9a9d2769a4ab64c7202767ac5bee5d1
Minh Duc <bdmduc3012000@gmail.com>
2022-08-06 23:57:34 +0700

    create class PromotionRequest

---
+++ src/main/java/vn/com/groupfive/tgdd/payload/request/PromotionRequest.java
@@ -0,0 +1,42 @@
+package vn.com.groupfive.tgdd.payload.request;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PromotionRequest {
+
+	@NotNull
+	@Min(value = 10)
+	@Max(value = 100)
+	private int promotionPercent;
+	
+	@NotNull
+	@JsonFormat(pattern ="yyyy-mm-dd")
+	private Date startDate;
+	
+	@NotNull
+	@JsonFormat(pattern ="yyyy-mm-dd")
+	private Date endDate;
+	
+	@NotNull
+	private boolean isActived;
+	
+	@NotNull
+	private List<Long> versionColorIds;
+	
+	@NotNull
+	private List<Long> provinceIds;
+}
